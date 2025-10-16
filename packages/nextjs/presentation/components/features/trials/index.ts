ad07be4df840cef92d028faff09a26065329aa0d
Oscar Fonseca <edsphinx@gmail.com>
2025-10-15 23:34:17 -0600

    refactor(nextjs): remove deprecated presentation layer
    - Remove old presentation/ folder components
    - Components moved to components/ folder in previous commits
    - Clean up duplicate component definitions
    - Part of architecture refactor to simplified component structure

--- packages/nextjs/presentation/components/features/trials/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Trial Components
- *
- * UI components for clinical trial interactions
- */
-
-export { ApplyButton } from './ApplyButton';
-export { StudyCard, StudyCardSkeleton, EmptyStudyCard } from './StudyCard';
-export { StudyList } from './StudyList';
